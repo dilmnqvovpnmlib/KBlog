41530d5815e2cef926e5c0fbaea1d2d97ad28232
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2020-09-17 02:06:11 +0900

    fix delete.sh

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,4 +1,4 @@
 docker ps -aq | xargs docker stop
 docker ps -aq | xargs docker rm
-docker images -aq | xargs docker rmi
+docker images -aq | xargs docker rmif
 docker volume ls -qf dangling=true | xargs -r docker volume rm
